e6d73fd4837071c1b5b07d500045bb7140b864ae
Andrew <verden.kma@gmail.com>
2021-04-18 11:03:02 +0300

    -style: digest thumbnails

--- src/main/frontend/src/digest/Thumbnail.tsx
+++ src/main/frontend/src/digest/Thumbnail.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import axios from 'axios'
 import {IAuthProps} from "../cms_backbone/CMSNavbarRouting";
 import {IMiniRecord} from "./Digest";
-import {Link} from "react-router-dom";
 
 interface IProps {
     auth: IAuthProps,
@@ -35,14 +34,11 @@ class Thumbnail extends React.Component<IProps, IState> {
     render() {
         return (
             <div className={"col-sm-6 col-md-3 col-lg-2 p-0"}>
-                <Link to={`/users/${this.props.data.publisher}/records/${this.props.data.recordOwnId}`}>
-                    {this.state.image &&
-                    <img className={"thumbnail-image"}
-                         src={"data:image/jpeg;base64, " + this.state.image}
-                         alt={`${this.props.data.caption}`}/>
-
-                    }
-                </Link>
+                <div className="digest-container">
+                    <a href={`/users/${this.props.data.publisher}/records/${this.props.data.recordOwnId}`}>
+                        <img src={"data:image/jpeg;base64, " + this.state.image} alt={`${this.props.data.caption}`}/>
+                    </a>
+                </div>
             </div>
         )
     }
